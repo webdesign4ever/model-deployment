324f12d43f89ebd51eb47d9441848cfa4616b12b
Muhammad Moazzam <rajamoazzam789@gmail.com>
2023-12-24 17:51:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -156,5 +156,5 @@ def predict():
         return jsonify({'error': 'Invalid request'})
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
     # app.run(debug=True)
